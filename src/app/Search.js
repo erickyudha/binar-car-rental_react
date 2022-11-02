7421f265e291957c6a283e8ce366d946388433b0
Erick Yudha <erickyudha18@gmail.com>
2022-11-02 16:52:03 +0700

    add search page root comp

--- src/app/Search.js
+++ src/app/Search.js
@@ -1,5 +1,32 @@
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Nav from "./components/Nav";
+import Overlay from "./components/Overlay";
+import CarForm from "./components/Search/CarForm";
+import "./Search.css"
+
 export default function Search() {
+    const handleSubmit = value => {
+        console.log('====================================');
+        console.log(value);
+        console.log('====================================');
+    }
+
     return (
-        <h1>UWEEE</h1>
+        <>
+            <Nav type="others" />
+            <Hero simple={true} />
+
+            <Overlay />
+
+            <section id="search">
+                <CarForm parentHandler={handleSubmit} />
+                <div id="search-result" className="flex-row flex-gap-24">
+
+                </div>
+            </section>
+
+            <Footer type="others" />
+        </>
     )
 }
